91f44087e3189a9180a4c6cb71f2c2eb051aa5ae
Vivian <vwangjianing@gmail.com>
2020-07-24 16:22:53 -0400

    allow user sign up

--- post/blog/admin.py
+++ post/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+# register db in the admin url
+admin.site.register(Post)
--- post/blog/views.py
+++ post/blog/views.py
@@ -1,28 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'My first post',
-        'content': 'Blog one content',
-        'date_created': 'August 5, 2019'
-    },
-    {
-        'author': 'Kelly White',
-        'title': 'Second post',
-        'content': 'Blog two content',
-        'date_created': 'March 17, 2020'
-    }
-]
-
-
-
+from .models import Post
 # Create your views here.
 
 def home(request):
     # render(request, subdirInTemplpate/filename)
-    context = {'posts': posts}
+    context = {'posts': Post.objects.all()}
     return render(request,'blog/home.html', context)
 
 
