6f88ebea39a3215b3d0f4b0ee85c81be45c20a6c
Vivian <vwangjianing@gmail.com>
2020-07-24 17:45:21 -0400

    allow sign in and sign out

--- post/users/views.py
+++ post/users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .form import UserRegisterForm
+
+
 # Create your views here.
 # classes converted to html, provided by Django
 
@@ -11,7 +14,12 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
+
