c2d5cb165140e6305789324410e0ed3fb8362385
Vivian <vwangjianing@gmail.com>
2020-07-27 14:15:18 -0400

    update and delete post

--- post/blog/views.py
+++ post/blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+
 # Create your views here.
 
 def home(request):
@@ -7,8 +16,53 @@ def home(request):
     context = {'posts': Post.objects.all()}
     return render(request,'blog/home.html', context)
 
-
-
 def about(request):
     return render(request,'blog/about.html')
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_invalid(self, form):
+        form.instance.author = self.request.author
+        return super().form_invalid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
+
+
+
 
--- post/users/views.py
+++ post/users/views.py
@@ -20,10 +20,21 @@ def register(request):
 
 @login_required
 def profile(request):
-    user_form = UserUpdateForm()
-    profile_form = ProfileUpdateForm()
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST,
+                                         request.FILES,
+                                         instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Account updated')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
     context = {
-        'user_form':user_form ,
+        'user_form':user_form,
         'profile_form':profile_form
     }
     return render(request, 'users/profile.html', context)
